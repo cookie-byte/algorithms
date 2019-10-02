7f69d7153726e300c0b731c8345b16665540b3c9
PRITHWIRAJ SAMANTA <prskid1000@gmail.com>
2019-10-02 17:16:49 +0530

    BST and HEAP
    Prithwiraj Samanta

---
+++ Data Structures/Graph/heap.cpp
@@ -0,0 +1,116 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+class heap
+{
+public:
+int count;
+vector<int>v;
+heap()
+{
+count=0;
+}
+void add(int value)
+{
+v.push_back(value);
+count++;
+minheapify();
+}
+void minheapify()
+{
+int i=count-1;
+while(i>0&&v[i]<v[(i-1)/2])
+{
+swap(i,(i-1)/2);
+i=(i-1)/2;
+}
+}
+void swap(int a,int b)
+{
+int t=v[a];
+v[a]=v[b];
+v[b]=t;
+}
+bool remove(int value)
+{
+int i=findindex(value);
+int left=0,right=0;
+if(i<0)
+{
+return false;
+}
+count--;
+v[i]=v[count];
+right=(2*i+2);
+left=(2*i+1);
+while(left<count&&(v[i]>v[left]||v[i]>v[right]))
+{
+if(v[left]<v[right])
+{
+i=left;
+right=(2*i+2);
+left=(2*i+1);
+}
+else
+{
+swap(right,left);
+}
+}
+return true;
+}
+void traverse()
+{
+for(int i=0;i<count;i++)cout<<v[i]<<"\n";
+}
+int findindex(int value)
+{
+	int u=count+1,l=0,mid;
+	while(l<=u)
+	{
+		mid=(u+l)/2;
+		if(value=v[mid])return mid;
+		else if(value<v[mid])u=mid-1;
+		else l=mid+1;
+	}
+	return -1;
+}
+bool search(int value)
+{
+int start=0,nodes=1;
+while(start<count)
+	{
+		start=nodes-1;
+		int end=nodes+start;
+		int c=0;
+		while(start<count&&start<end)
+		{
+			if(value==v[start])
+			{
+				return true;
+			}
+			else if(value>v[(start-1)/2]&&value<v[start])
+			{
+				c++;
+			}
+			start=start+1;
+		}
+		if(c==nodes)return false;
+		nodes=nodes*2;
+	}
+	return false;
+}
+};
+int main()
+{
+heap h;
+h.add(5);
+h.add(8);
+h.add(1);
+h.add(2);
+h.add(3);
+h.add(7);
+h.add(9);
+h.add(12);
+cout<<h.findindex(12)<<"\n";
+return 0;
+}
---
+++ Data Structures/Tree/bst.cpp
@@ -0,0 +1,209 @@
+﻿#include <iostream>
+using namespace std;
+class node
+{
+	public:
+	int data;
+	node *left,*right;
+	node()
+	{
+		left=nullptr;
+		right=nullptr;
+		data=0;
+	}
+};
+class bst
+{
+	public:
+	node *root;
+	int count;
+	bst()
+	{
+		root=nullptr;
+		count=0;
+	}
+	void insert(int v)
+	{
+		if(root==nullptr)
+		{
+			root=new node();
+			(*root).data=v;
+			count++;
+		}
+		else
+		{
+			insert(&(*root),v);
+		}
+	}
+	void insert(node *current,int v)
+	{
+		if((*current).data>v)
+		{
+			if((*current).left==nullptr)
+			{
+				(*current).left=new node();
+				(*((*current).left)).data=v;
+				count++;
+			}
+			else
+			{
+				insert(&(*((*current).left)),v);
+			}
+		}
+		else
+		{
+			if((*current).right==nullptr)
+			{
+				(*current).right=new node();
+				(*((*current).right)).data=v;
+				count++;
+			}
+			else
+			{
+				insert(&(*((*current).right)),v);
+			}
+		}
+	}
+	void preorder()
+	{
+		preorder(&(*root));
+	}
+	void preorder(node *current)
+	{
+		if(current!=nullptr)
+		{
+		cout<<(*current).data<<"\n";
+		preorder(&(*((*current).left)));
+		preorder(&(*((*current).right)));
+		}
+	}
+	void inorder()
+	{
+		inorder(&(*root));
+	}
+	void inorder(node *current)
+	{
+		if(current!=nullptr)
+		{
+			inorder(&(*((*current).left)));
+			cout<<(*current).data<<"\n";
+			inorder(&(*((*current).right)));
+		}
+	}
+	void postorder()
+	{
+		postorder(&(*root));
+	}
+	void postorder(node *current)
+	{
+		if(current!=nullptr)
+		{
+			postorder(&(*((*current).left)));
+			postorder(&(*((*current).right)));
+			cout<<(*current).data<<"\n";
+		}
+	}
+	node* findparent(int value)
+	{
+		return findparent(value,&(*root));
+	}
+	node* findparent(int value,node *current)
+	{
+		if(current==nullptr)
+		{
+			return nullptr;
+		}
+		else if((*current).data>value)
+		{
+			if((*current).left==nullptr)return nullptr;
+			else if((*((*current).left)).data==value)
+			return &(*current);
+			else return findparent(value,&(*((*current).left)));
+		}
+		else
+		{
+			if((*current).right==nullptr)return nullptr;
+			else if((*((*current).right)).data==value)
+			return &(*current);
+			else return findparent(value,&(*((*current).right)));
+		}
+	}
+	node* findnode(int value)
+	{
+		return findnode(&(*root),value);
+	}
+	node* findnode(node *current,int value)
+	{
+		if(current==nullptr)return nullptr;
+		else if((*current).data==value)return &(*current);
+		else if((*current).data>value)return findnode((*current).left,value);
+		else return findnode((*current).right,value);
+	}
+	bool search(int value)
+	{
+		return search(&(*root),value);
+	}
+	bool search(node *current,int value)
+	{
+		if(current==nullptr)return false;
+		else if((*current).data==value)return true;
+		else if((*current).data>value)return findnode((*current).left,value);
+		else return findnode((*current).right,value);
+	}
+	bool remove(int value)
+	{
+		node *rv=&(*findnode(value));
+		if(rv==nullptr)
+		return false;
+		node *p=findparent(value);
+		if(count==1)root=nullptr;
+		else if((*rv).left==nullptr and (*rv).right==nullptr)
+		{
+			if((*rv).data<(*p).data)
+			(*p).left=nullptr;
+			else (*p).right=nullptr;
+		}
+		else if((*rv).left==nullptr and (*rv).right!=nullptr)
+		{
+			if((*rv).data<(*p).data)
+			(*p).left=&(*((*rv).right));
+			else (*p).right=&(*((*rv).right));
+		}
+		else if((*rv).left!=nullptr and (*rv).right==nullptr)
+		{
+			if((*rv).data<(*p).data)
+			(*p).left=&(*((*rv).left));
+			else (*p).right=&(*((*rv).left));
+		}
+		else
+		{
+			node *l=&(*((*rv).left));
+			while((*l).right!=nullptr)
+			{
+				l=&(*((*l).right));
+			}
+			(*findparent((*l).data)).right=nullptr;
+			(*rv).data=(*l).data;
+		}
+		count--;
+		return true;
+	}
+};
+int main()
+{
+	bst tree;
+	tree.insert(5);
+	tree.insert(7);
+	tree.insert(2);
+	tree.insert(8);
+	tree.insert(6);
+	tree.insert(3);
+	tree.insert(4);
+	tree.insert(1);
+	tree.insert(10);
+	tree.insert(9);
+	tree.preorder();
+	cout<<tree.remove(4)<<"\n";
+	tree.preorder();
+	return 0;
+}
